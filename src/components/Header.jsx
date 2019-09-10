2e3d1788bb33b28fc28c0c0bb8cabc298348682a
Camille Charpentier <cam.charpentier@live.fr>
2019-09-10 18:23:54 +0200

    change page is ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
+// redux
+import { connect } from 'react-redux';
+import { goToPagePokemons, goToPageFavourites } from '../actions/actions';
+
 class Header extends Component {
 	render() {
 		return (
 			<div className="header">
-				<NavLink to="/" /*onClick={this.props.getNextPokemons}*/>
+				<NavLink to="/" onClick={this.props.goToPagePokemons}>
 					<h2>Pokemons</h2>
 				</NavLink>
-				<NavLink to="/fav" /*onClick={this.props.getNextPokemons(this.props.next)}*/>
+				<NavLink to="/fav" onClick={this.props.goToPageFavourites}>
 					<h2>Your favourites</h2>
 				</NavLink>
 			</div>
@@ -16,5 +20,17 @@ class Header extends Component {
 	}
 }
 
+const mapStateToProps = (state) => {
+	return {
+		view: state.view
+	};
+};
 
-export default Header;
+const mapDispatchToProps = (dispatch) => {
+	return {
+		goToPagePokemons: () => dispatch(goToPagePokemons()),
+		goToPageFavourites: () => dispatch(goToPageFavourites())
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
